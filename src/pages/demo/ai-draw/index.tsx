84f3dc4068c76b59f47cd0e766917e951b100afd
Nacho.L <nacho.liu@sjfood.com>
2024-08-13 17:40:37 +0800

    Update ai draw

--- src/pages/demo/ai-draw/index.tsx
+++ src/pages/demo/ai-draw/index.tsx
@@ -1,5 +1,6 @@
-import { Button, Input } from "antd";
-import { useState } from "react";
+import { LoadingOutlined } from "@ant-design/icons";
+import { Button, Input, Spin } from "antd";
+import { useEffect, useState } from "react";
 
 export const AiDraw = () => {
   const baseUrl = "https://api-flux1.api4gpt.com/?prompt=";
@@ -9,8 +10,8 @@ export const AiDraw = () => {
   const [imageStatus, setImageStatus] = useState<string>("");
 
   const handleGenerate = () => {
-    setImageUrl(`${baseUrl}${prompt}`);
     setImageStatus("loading");
+    setImageUrl(`${baseUrl}${prompt}?time=${Date.now()}`);
   };
 
   const handleImageLoad = () => {
@@ -21,24 +22,57 @@ export const AiDraw = () => {
     setImageStatus("error");
   };
 
+  // 超时处理
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      if (imageStatus === "loading") {
+        setImageStatus("error");
+      }
+    }, 30000);
+
+    return () => clearTimeout(timer);
+  }, [imageStatus]);
+
   return (
     <div className="p-4">
-      <h1>AI Draw</h1>
-      <div className="flex gap-4 items-center">
-        prompt:
-        <Input
+      <h1>AI Draw 画图</h1>
+      <h2>
+        提示词需要英文且越详细越好～{" "}
+        <a href="https://www.deepl.com/zh/translator/l/en/en">👉🏻Deepl翻译</a>
+      </h2>
+      <div className="flex gap-4">
+        <span className="h-[2rem] leading-8 text-nowrap">prompt(提示词):</span>
+        <Input.TextArea
           value={prompt}
+          className="max-h-[10rem]"
           onChange={(e) => setPrompt(e.target.value)}
           placeholder="Enter prompt"
           onPressEnter={handleGenerate}
         />
         <Button type="primary" onClick={handleGenerate}>
-          Generate
+          生成图片
         </Button>
       </div>
       <div className="w-3/5 aspect-square p-8 mx-auto">
-        {imageStatus === "loading" && <p>Loading...</p>}
-        {imageStatus === "error" && <p>Error loading image</p>}
+        {imageStatus !== "loaded" && (
+          <div className="size-full shadow-xl rounded-2xl p-4 flex flex-col items-center justify-center">
+            {imageStatus === "loading" && (
+              <>
+                <Spin
+                  indicator={<LoadingOutlined style={{ fontSize: 48 }} spin />}
+                />
+                <p>Loading...</p>
+              </>
+            )}
+            {imageStatus === "error" && (
+              <>
+                <div>暂无图片数据</div>
+                <div>请输入prompt生成图片或更换prompt</div>
+                <div>或者等待一会再试试</div>
+              </>
+            )}
+          </div>
+        )}
         <img
           src={imageUrl}
           alt="AI Draw"
